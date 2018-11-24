c772bd0a0e88e4c9fb13ecad7e56ad60859e6e40
DeVeL_AnOnIk <27363518+anonik9900@users.noreply.github.com>
2018-11-25 00:45:46 +0100

    main files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# sharuby-client
+# Sharuby - Client
+
 Sharuby - Client is the Ruby-app based on Sharuby Library developed for all.
+
+## Dependency:
+
+[Sharuby By Anonik][https://github.com/anonik9900"sharuby]
+[Inifile By Tim Pease][https://rubygems.org/gems/inifile/versions/3.0.0]
+
+## Compatibility:
+
+Compatible on: `Windows` and `Linux`.
+
+Language-Aviable: `Italian-Italiano (It-it)`, `English-Inglese (En-en)`.
---
+++ __Linux/Sharuby-Client-Linux.rb
@@ -0,0 +1,125 @@
+require 'sharuby'
+
+
+puts print"*************************"
+puts print "Sharuby Client v.0.2.5"
+puts
+puts print"  Writted by Anonik     "
+puts
+puts print"*************************"
+puts
+
+puts print "Select the Language..."
+puts
+puts print "English | Italian"
+puts
+ui_language = gets.chomp
+
+#Italian - Windows
+case ui_language
+    
+    when "Italian","italian","it","IT","It","Italiano","italiano"
+
+    puts
+    puts print "Scrivi il nome del tuo file (senza estensione) "
+    puts
+    file_name = gets.chomp
+    
+    puts
+    puts print "Scrivi l'estensione del tuo file"
+    puts
+    file_ext = gets.chomp
+    
+    puts
+    puts print "Scegli la tipologia di algoritmo"
+    puts
+    puts print"1)SHA1 | 2)SHA256 | 3)MD5"
+    puts
+    alg_check = gets.chomp
+
+    
+    if alg_check == 1.to_s
+        sha1result = Sharuby::SHA1.hash(file_name + file_ext)
+        puts "SHA1 = "+ sha1result
+        ok = gets.chomp
+    end#Fine SHA1
+    
+    if alg_check == 2.to_s
+        sha256result = Sharuby::SHA256.hash(file_name + file_ext)
+        puts "SHA256 = " + sha256result
+        ok = gets.chomp
+    end#Fine SHA256
+    
+    if alg_check == 3.to_s
+        md5result = Sharuby::MD5.hash(file_name + file_ext)
+        puts "MD5 = " + md5result
+        ok = gets.chomp
+    end#Fine MD5
+    
+    if alg_check >= 4.to_s
+        puts print "Il numero non corrisponde a nessuna scelta, premi invio per riavviare."
+        ok = gets.chomp
+    end#Errore
+    
+   
+    
+    end#Fine Italiano
+#End Italian Windows
+
+
+
+#------------------------------------------------------------------------ E N G L I S H -----------------------------------------------------------------------------
+
+
+#English - Windows
+case ui_language
+
+    when "English","english","En","en","EN","eN","USA","Usa","Inglese","inglese"
+
+    puts
+    puts print "Write the name of your file (without extension) "
+    puts
+    file_name = gets.chomp
+    
+    puts
+    puts print "Write the extension of your file "
+    puts
+    file_ext = gets.chomp
+    
+    puts
+    puts print "Choose the type of algorithm"
+    puts
+    puts print"1)SHA1 | 2)SHA256 | 3)MD5"
+    puts
+    alg_check = gets.chomp
+
+    
+    if alg_check == 1.to_s
+        sha1result = Sharuby::SHA1.hash(file_name + file_ext)
+        puts "SHA1 = "+ sha1result
+        ok = gets.chomp
+    end#Fine SHA1
+    
+    if alg_check == 2.to_s
+        sha256result = Sharuby::SHA256.hash(file_name + file_ext)
+        puts "SHA256 = " + sha256result
+        ok = gets.chomp
+    end#Fine SHA256
+    
+    if alg_check == 3.to_s
+        md5result = Sharuby::MD5.hash(file_name + file_ext)
+        puts "MD5 = " + md5result
+        ok = gets.chomp
+    end#Fine MD5
+    
+    
+    if alg_check >= 4.to_s
+        puts print "The number does not correspond to any choice, press enter to restart."
+        ok = gets.chomp
+    end#Error
+    
+   
+    
+    end#End English
+
+#End English Windows
